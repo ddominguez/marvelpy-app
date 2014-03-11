43ec58b3542e933859095fa70c8e17580dc92d5c
David Dominguez <ddominguez@gmail.com>
2014-03-10 21:48:00 -0400

    get character info and display in template
    * added index template

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+import settings
+from flask import Flask, render_template
+from marvelpy import Marvel
+
 app = Flask(__name__)
+app.config['DEBUG'] = settings.DEBUG
 
 
 @app.route('/')
 def index():
-    return 'A simple marvel app using the marvelpy library.'
+    # Gambit!
+    character = marvel.characters(id=1009313)
+    data = character.json()
+    result = data['data']['results'][0]
+    return render_template(
+        'index.html',
+        attributionHTML=data['attributionHTML'],
+        result=result
+    )
 
 if __name__ == '__main__':
+    marvel = Marvel(
+        api_key=settings.MARVEL_API_KEY,
+        private_key=settings.MARVEL_PRIVATE_KEY
+    )
     app.run()
