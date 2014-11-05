190a96dc62e04381dc8539c14eff16a46d129f23
David Dominguez <ddominguez@gmail.com>
2014-11-05 18:39:28 -0500

    effin typo

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 # get data environment variables
-DEBUG = os.environ.get('FlASK_DEBUG', False)
+DEBUG = os.environ.get('FLASK_DEBUG', False)
 PORT = int(os.environ.get('PORT', 5000))
 MARVEL_API_KEY = os.environ.get('MARVEL_API_KEY', '')
 MARVEL_PRIVATE_KEY = os.environ.get('MARVEL_PRIVATE_KEY', '')
